3e1ade4f9b79c403cd166e41b54d7e732a8b30df
SerkanPN <123215352+SerkanPN@users.noreply.github.com>
2026-03-20 13:43:29 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ export default function App() {
           <Route path="profile" element={<Profile />} />
           <Route path="seller" element={<SellerDashboard />} />
           <Route path="wishlist" element={<Wishlist />} />
-          <Route path="*" element={<div className="container mx-auto px-4 py-24 text-center"><h1 className="text-4xl font-bold uppercase">Coming Soon</h1></div>} />
+          <Route path="*" element={<div className="container mx-auto px-4 py-24 text-center h-screen flex flex-col justify-center"><h1 className="text-4xl font-bold uppercase tracking-tighter italic">404 - Dimension Not Found</h1></div>} />
         </Route>
       </Routes>
     </BrowserRouter>
